34b2bd3d593214e4752537155c5e7285f3b859ff
Ingeborg Eldøy <ieldoy@gmail.com>
2021-12-23 15:58:35 +0100

    First try w/JS - not working

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,31 @@
 module.exports = async function($) {
   $.page.title = 'To-do-list'
 
+  document.querySelector('#push').onclick = function() {
+    if(document.querySelector('#newtask input').value.length == 0) {
+      alert("Please enter a task")
+  }
+
+  else {
+    document.querySelector('#tasks').innerHTML +=
+    /* html*/`
+      <div class="task">
+        <span id="taskname">
+          ${document.querySelector('#newtask input').value}
+          </span>
+          <button class="delete">
+            <i class="far fa-trash-alt"></i>
+          </button>
+      </div>
+    `
+        var current_tasks = document.querySelectorAll(".delete")
+        for(var i = 0; i < current_tasks.length; i++) {
+          current_tasks[i].onclick = function() {
+            this.parentNode.remove()
+          }
+        }
+      }
+
 return /* html */`
 
 <div class='container'>
@@ -16,5 +41,6 @@ return /* html */`
 <div id='tasks'>
 </div>
 
-  `
+ `
+}
 }
